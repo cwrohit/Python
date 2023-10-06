94bf230ecb081919ec2ae2d720b23cf2794a0d41
Rohit Yadav <122785331+cwrohit@users.noreply.github.com>
2023-10-06 19:44:41 +0530

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,17 @@
+# "" this is use for  string data type
+print(type("ROHIT"))
+#'' this also use for string data type
+print(type('rohit'))
+print(type("rohit"))
+#""" this also use for string data type
+print(type("""ROHIT"""))
+#''' use for string data type
+print(type('''rohit'''))
+
+print("python is sa easy 'langauge'")
+print('we can easy to unederstand"python"')
+
+
+print("""hlo bhai kaise ho
+      tum sab
+      thik ho""")
